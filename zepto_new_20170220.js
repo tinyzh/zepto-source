b0b0c02f57564fcccf81cc7af00f57f920846998
zhangchao <zc_820@163.com>
2017-03-02 17:33:58 +0800

    add

--- zepto_new_20170220.js
+++ zepto_new_20170220.js
@@ -68,7 +68,65 @@ var Zepto = (function(){
 		if(temp) (parent = tempParent).appendChild(element);
 
 		match = ~zepto.qsa(parent, selector).indexOf(element);
+
+		temp && tempParent.removeChild(element);
+
+		return match;
 	}
+
+	function type(obj){
+		return obj == null ? String(obj) : class2type[toString.call(obj)] || 'object'
+	}
+	function isFunction(value){return type(value) == 'function'}
+	function isWindow(obj){ return obj != null && obj == obj.window }
+	function isDocument(obj){ return obj != null && obj.nodeType == obj.DOCUMENT_NODE }
+	function isObject(obj){
+		return type(obj) == 'object'
+	}
+
+	function isPlainObject(obj){
+		return isObject(obj) && !isWindow(obj) && Object.getPrototypeOf(obj) == Object.prototype
+	}
+
+	//伪数组、数组   方法也有length属性，为参数个数
+	function likeArray(obj){ return typeof obj.length == 'number' }
+
+	function compact(array){ return filter.call(array,function(item){ return item != 'null' }) }
+
+	function flatten(array){
+		return array.length > 0 ? $.fn.concat.apply([], array) : array;
+	}
+
+	camelize = function(str){
+		return str.replace(/-+(.)?/g,function(match,chr){
+			return chr ? chr.toUpperCase() : '';
+		})
+	}
+
+	function dasherize(str){
+		return str.replace(/::/g,'/')
+					.replace(/([A-Z]+)([A-Z][a-z])/g,'$1_$2')//大小写之间插入下划线(_)，比如AAAbb->AAA_bb
+					.replace(/([a-z\d])([A-Z])/g,'$1_$2')//在大写字母前插入下划线，比如 bb123AA->bb123_AA
+					.replace(/_/g,'-')//将下划线替换为'-'
+					.toLowerCase() //转换为小写
+	}
+
+	uniq = function(array){
+		return filter.call(array,function(item,idx){
+			return array.indexOf(item) == idx;
+		})
+	}
+
+	function classRE(name){
+		return name in classCache ? classCache[name] : (classCache[name] = new RegExp('(|\\s)' + name + '(\\s|$)'))
+	}
+
+	function maybeAddPx(name,value){
+		return 
+	}
+
+
+
 
 
 
