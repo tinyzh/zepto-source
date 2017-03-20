a4d26dff1a0bd146f637408b5d20e530f54786ae
zhangchao <zc_820@163.com>
2017-03-20 16:45:16 +0800

    update

--- zepto_new_20170220.js
+++ zepto_new_20170220.js
@@ -260,9 +260,266 @@ var Zepto = (function(){
             isSimple = simpleSelectorRe.test(nameOnly);
         
         return (element.getElementById && isSimple && maybeID) ?
-            ( (found = element.getElementById(nameOnly)) ? [found] : [] )
+            ( (found = element.getElementById(nameOnly)) ? [found] : [] ) :
+            (element.nodeType !== 1 && element.nodeType !== 9 && element.nodeType !== 11) ? [] :
+             slice.call(
+                 isSimple && !maybeID && element.getElementsByClassName ?
+                     maybeClass ? element.getElementsByClassName(nameOnly) :
+                         element.getElementsByTagName(selector) :
+                     element.querySelectorAll(selector);
+             )
         
     }
+
+    function filtered(nodes, selector){
+        return selector == null ? $(nodes) : $(nodes).filter(selector);
+    }
+
+    $.contains = document.documentElement.contains ?
+        function(parent,node){
+            return parent != node && parent.contains(node)
+        } :
+        function(parent, node){
+            while (node && (node = node.parentNode)){
+                if (node === parent) return true;
+            }
+            return false;
+        }
+
+    function funcArg(context, arg, idx, payload){
+        return isFunction(arg) ? arg.call(context, idx, payload) : arg;
+    }
+
+    function setAttribute(node, name, value){
+        value == null ? node.removeAttribute(name) : node.setAttribute(name, value);
+    }
+
+    function className(node,value){
+        var klass = node.className || '',
+            svg = klass && klass.baseVal !== undefined;
+
+        if(value === undefined) return svg ? klass.baseVal : klass
+        svg ? (klass.baseVal = value) : (node.className = value)
+    }
+
+    function deserializeValue(value){
+        try{
+            return value ?
+                value == "true" ||
+                ( value == "false" ? false :
+                  value == "null" ? null :
+                  +value + "" == value ? +value :
+                  /^[\[\{]/.test(value) ? $.parseJSON(value) :
+                  value
+                ) : value
+        }catch(e){
+            return value;
+        }
+    }
+    }
+
+    $.type = type;
+    $.isFunction = isFunction;
+    $.isWindow = isWindow;
+    $.isArray = isArray;
+    $.isPlainObject = isPlainObject;
+
+    $.isEmptyObject = function(obj){
+        var name;
+        for(name in obj) return false;
+        return true;
+    }
+
+    $.inArray = function(elem, array, i){
+        return emptyArray.indexOf.call(array,elem,i);
+    }
+
+    $.camelCase = function(str){
+        return str.replace(/-+(.)?/g,function(match, chr){
+            return chr ? chr.toUpperCase() : '';
+        })
+    }
+
+    $.trim = function(str){
+        return str == null ? "" : String.prototype.trim.call(str);
+    }
+
+    $.uuid = 0;
+    $.support = { };
+    $.expr = { }
+    $.noop = function(){};
+
+    $.map = function(elements, callback){
+        var value, values = [], i , key;
+        if(likeArray(elements)){
+            for(i = 0; i < elements.length; i++){
+                value = callback(elements[i],i)
+                if(value != null) values.push(value);
+            }
+        }else{
+            for(key in elements){
+                value = callback(elements[key],key);
+                if(value != null) values.push(value);
+            }
+        }
+
+        return flatten(values);
+    }
+
+    $.each = function(elements, callback){
+        var i, key;
+        if(likeArray(elements)){
+            for(i = 0; i< elements.length; i++){
+                if(callback.call(elements[i],i,elements[i]) === false) return elements;
+            }
+        }else{
+            for(key in elements){
+                if(callback.call(elements[key],key,elements[key]) === false) return elements;
+            }
+        }
+        return elements;
+    }
+
+    $.grep = function(elements, callback){
+        return filter.call(elemetns,callback);
+    }
+
+    if(window.JSON) $.parseJSON = JSON.parse;
+
+    $.each("Boolean Number String Array Date RegExp Object Error".split(" "),function(i,name){
+        class2type["[object "+ name +"]"] = name.toLowerCase();
+    })
+
+    $.fn = {
+        constructor : zepto.Z,
+        length : 0,
+        forEach : emptyArray.forEach,
+        reduce : emptyArray.reduce,
+        push : emptyArray.push,
+        sort : emptyArray.sort,
+        splice : emptyArray.splice,
+        indexOf : emptyArray.indexOf,
+
+        concat : function(){
+            var i, value, args = [];
+            for(i = 0; i < arguments.length; i++){
+                value = arguments[i];
+                args[i] = zepto.isZ(this) ? value.toArray() : value;
+            }
+            return concat.apply(zepto.isZ(this) ? this.toArray() : this, args);
+        },
+        
+        map : function(fn){
+            return $($.map(this,function(el,i){return fn.call(el,i,el)}))
+        },
+        
+        slice : function(){
+            return $(slice.apply(this, arguments))
+        },
+        
+        ready : function(callback){
+            if(readyRE.test(document.readyState) && document.body) callback($)
+            else document.addEventListener('DOMContentLoaded',function(){ callback($); },false)
+
+            return this;
+        },
+
+        get : function(idx){
+            return idx === undefined ? slice.call(this) : this[idx >= 0 ? idx : idx + this.length];
+        },
+
+        toArray : function(){
+            return this.get();
+        },
+
+        size : function(){
+            return this.length;
+        },
+
+        remove : function(){
+            return this.each(function(){
+                if(this.parentNode != null){
+                    this.parentNode.removeChild(this);
+                }
+            })
+        },
+
+        each : function(callback){
+            emptyArray.every.call(this,function(el,idx){
+                return callback.call(el,idx,el) !== false;
+            });
+
+            return this;
+        },
+
+        filter : function(selector){
+            if(isFunction(selector)) return this.not(this.not(selector));
+
+            return $(filter.call(this,function(element){
+                return zepto.matches(element,selector);
+            }))
+        },
+        add : function(selector,context){
+            return $(uniq(this.concat($(selector,context))))
+        },
+        is : function(selector){
+            return this.length ? 0 && zepto.matches(this[0], selector);
+        },
+        not : function(selector){
+            var nodes = [];
+            if(isFunction(selector) && selector.call !== undefined){
+                this.each(function(idx){
+                    if(!selector.call(this,idx)) nodes.push(this);
+                })
+            }else{
+                var excludes = typeof selector == 'string' ? this.filter(selector) :
+                    (likeArray(selector) && isFunction(selector.item)) ? slice.call(selector) : $(selector);
+
+                this.forEach(function(el){
+                    if(excludes.indexOf(el) < 0) nodes.push(el);
+                })
+            }
+
+            return $(nodes)
+        },
+        has : function(selector){
+            return this.filter(function(){
+                return isObject(selector) ? $.contains(this, selector) : $(this).find(selector).size()
+            })
+        },
+        eq : function(idx){
+            return idx === -1 ? this.slice(idx) : this.slice(idx, +idx + 1);
+        },
+        first : function(){
+            var el = this[0]
+            return el && !isObject(el) ? el : $(el);
+        },
+        last : function(){
+            var el = this[this.length - 1];
+            return el && !isObject(el) ? el : $(el);
+        },
+        find : function(selector){
+            var result, $this = this;
+            if(!selector) result = $()
+            else if (typeof selector == 'object')
+                result = $(selector).filter(function(){
+                    var node = this;
+                    return emptyArray.some.call($this,function(parent){
+                        return $.contains(parent,node)
+                    })
+                })
+            else if(this.length == 1) result = $(zepto.qsa(this[0],selector))
+            else result = this.map(function(){ return zepto.qsa(this, selector) })
+            return result;
+        }
+        
+        
+
+
+    }
+
+
+
 
 
 
